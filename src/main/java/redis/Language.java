5f9bf02fe69a6ef3c9d9d5eb29866da45f38a179
xxXRatJuiceXxx <zhenechka.chernykh.05@mail.ru>
2023-01-17 23:17:49 +0300

    FirstCommit

---
+++ src/main/java/redis/Language.java
@@ -0,0 +1,35 @@
+package redis;
+
+import java.math.BigDecimal;
+
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+    //Getters and Setters omitted
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(Boolean official) {
+        isOfficial = official;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+}
